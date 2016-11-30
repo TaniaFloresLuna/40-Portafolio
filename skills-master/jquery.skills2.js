b1d197293d0f4e133b31c8f686ba6cb51d6bd232
TaniaFloresLuna <tanialuna08920@gmail.com>
2016-11-29 23:49:11 -0600

    Portafolio versión 3

---
+++ skills-master/jquery.skills2.js
@@ -0,0 +1,44 @@
+$.fn.skill = function() {
+
+	mSkill = this;
+
+	mSkill.find('.skillBar').each(function() {
+
+		if( true) {
+			//una vez que cada skill bar esta en el viewport
+			
+			$(this).addClass('sk-fired'); //agregamos una clase como bandera para evitar que se vuelva a reproducir la animacion
+			var defaultPercentage = "50%";
+			var color = $(this).attr('skill-color');
+			var defaultColor = "white";
+			//animamos el ancho de cada barra
+			if($(this).attr('skill-percentage')) {
+				$(this).width($(this).attr('skill-percentage'));
+			} else {
+				$(this).width(defaultPercentage);
+			}
+
+			//seteamos el color
+
+			if(color) {
+				$(this).css('background-color', color);
+			} else {
+				$(this).css('background-color',defaultColor);
+			}
+
+			//buscamos las imagenes para animarlas
+			$(this).parent().find(".skill-image").each(function() {
+				var imagen = $(this);
+				setInterval(function() {
+
+					imagen.show().addClass("animated").addClass("bounceIn");
+				}, 2000);
+				
+			});
+			}
+		});
+
+
+
+     return mSkill;
+}
